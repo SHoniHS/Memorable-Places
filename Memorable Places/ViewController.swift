47f8577e39cd08b53aae5c6abb9e27a7060d20f2
SHS <SHS@macbook-pro-de-shs.local>
2019-01-09 21:40:15 +0100

    Initial Commit

---
+++ Memorable Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memorable Places
+//
+//  Created by SHS on 9/1/19.
+//  Copyright © 2019 SHS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
